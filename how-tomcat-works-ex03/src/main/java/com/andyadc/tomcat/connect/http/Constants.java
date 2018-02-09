db6acc8c51b66373f57ca992a6d34d5e6906532d
99BILL\andy.an <andy.an@99bill.com>
2018-02-09 15:31:19 +0800

    How Tomcat Works

---
+++ how-tomcat-works-ex03/src/main/java/com/andyadc/tomcat/connect/http/Constants.java
@@ -0,0 +1,8 @@
+package com.andyadc.tomcat.connect.http;
+
+/**
+ * @author andy.an
+ * @since 2018/2/9
+ */
+public class Constants {
+}
--- how-tomcat-works-ex03/src/main/java/com/andyadc/tomcat/connect/http/HttpConnector.java
+++ how-tomcat-works-ex03/src/main/java/com/andyadc/tomcat/connect/http/HttpConnector.java
@@ -1,8 +1,51 @@
 package com.andyadc.tomcat.connect.http;
 
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author andy.an
  * @since 2018/2/9
  */
-public class HttpConnector {
+public class HttpConnector implements Runnable {
+
+    volatile boolean stopped;
+    private String scheme = "http";
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    @Override
+    public void run() {
+        ServerSocket serverSocket = null;
+        int port = 8080;
+
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        while (!stopped) {
+            Socket socket = null;
+
+            try {
+                socket = serverSocket.accept();
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            //TODO
+
+        }
+    }
+
+    public void start() {
+        Thread thread = new Thread(this);
+        thread.start();
+    }
 }
